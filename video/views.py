c1634e6abb4248f9b1388ba9973eb96fc0f1a633
Matthew Makai <matthew.makai@gmail.com>
2015-05-11 14:16:58 -0700

    product page now enabled

--- video/views.py
+++ video/views.py
@@ -1,5 +1,5 @@
 import requests
-from flask import render_template
+from flask import render_template, request
 
 from . import app, db
 from .models import SupportTicket
@@ -18,13 +18,15 @@ def index():
     resp = requests.get(SWAPI)
     if resp.status_code is not 200:
         return render_template('500.html')
-    ships = resp.json()
-    return render_template('index.html', ships=ships['results'])
+    products = resp.json()
+    return render_template('index.html', products=products['results'])
 
 
 @app.route('/products')
 def products():
-    return render_template('product.html', product=product)
+    swapi_ship_url = request.args.get('url', '')
+    results = requests.get(swapi_ship_url)
+    return render_template('product.html', product=results.json())
 
 
 @app.route('/tickets', methods=['GET', 'POST'])
