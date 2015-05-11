138d9885b49a9d1f74745807152f4edf1b429091
Matthew Makai <matthew.makai@gmail.com>
2015-05-11 13:39:45 -0700

    got front page up and running

--- video/views.py
+++ video/views.py
@@ -1,18 +1,30 @@
+import requests
 from flask import render_template
-
 
 from . import app, db
 from .models import SupportTicket
 
+# List endpoint for the starwars ship API
+SWAPI = 'http://swapi.co/api/starships'
+
+
+@app.route('/landing')
+def landing():
+    return render_template('landing.html')
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    resp = requests.get(SWAPI)
+    if resp.status_code is not 200:
+        return render_template('500.html')
+    ships = resp.json()
+    return render_template('index.html', ships=ships['results'])
 
 
 @app.route('/products')
 def products():
-    pass
+    return render_template('product.html', product=product)
 
 
 @app.route('/tickets', methods=['GET', 'POST'])
@@ -25,8 +37,4 @@ def tickets():
 def ticket():
     pass
 
-
-@app.route('/landing')
-def landing():
-    return render_template('landing.html')
 
